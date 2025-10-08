77f4fc4fdeb7d88f1faef6020a1d87f3b3622f21
Gediminas Survila <survila.gediminas@gmail.com>
2025-10-08 15:27:55 +0300

    app url

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.example.cappwatest",
-  appName: "Capacitor PWA Test",
+  appId: "com.example.capacitorcroossplatform",
+  appName: "Capacitor CrossPlatform",
   webDir: "dist",
   server: {
     // For iOS native build, point this to your deployed PWA URL
-    url: "https://pwa-capacitor.netlify.app",
+    url: "https://capacitor-crossplatform.netlify.app",
     cleartext: true,
   },
   plugins: {
